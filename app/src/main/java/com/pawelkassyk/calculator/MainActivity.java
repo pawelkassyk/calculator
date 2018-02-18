1f421a308e757bc87063b8b43b237f5a3e13755d
Paweł Kassyk <pkassyk@gmail.com>
2018-02-18 13:16:09 +0100

    Displaying number 7

--- app/src/main/java/com/pawelkassyk/calculator/MainActivity.java
+++ app/src/main/java/com/pawelkassyk/calculator/MainActivity.java
@@ -2,12 +2,43 @@ package com.pawelkassyk.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button7, button8, button9, buttonTimes, button4, button5, button6, buttonMinus,
+            button1, button2, button3, buttonPlus, buttonC, button0, buttonDivide, buttonEquals;
+
+    private TextView screen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        screen = findViewById(R.id.screen);
+
+
+        button7 = findViewById(R.id.button7);
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newValue;
+                if (screen.getText().toString().equals("0")) {
+                    newValue = "7";
+                } else {
+                    newValue = screen.getText().toString() + "7";
+                }
+
+                screen.setText(newValue);
+            }
+        });
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
     }
 }
